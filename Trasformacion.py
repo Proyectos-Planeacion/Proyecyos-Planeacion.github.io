bf18b550d0eb7ca166adf7ee3145ad714b1cc3c8
Valerius9515 <skody123@gmail.com>
2024-06-11 21:17:40 -0500

    c5

--- Trasformacion.py
+++ Trasformacion.py
@@ -1,5 +1,6 @@
 import mysql.connector, pandas as pd
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, \
+    request
 
 
 conexion = mysql.connector.connect(
@@ -13,9 +14,9 @@ port="3306"
 
 
 df = pd.read_sql('SELECT * FROM flor_en_transito', con=conexion)
-df = df[df["Producto"] == "ROSE"]
+# df = df[df["Producto"] == "ROSE"]     
 df = df[df["Flor Por Ingresar"] != 0]
-Productos = " <option value="">" + df["Producto"].unique() + "</option>"
+Productos = " <option value="  + df["Producto"].unique() + ">" + df["Producto"].unique() + "</option>"
 
 df = df.groupby(['Producto', 'Color', 'Grado'])['Flor Por Ingresar'].sum()
 print(df)
@@ -37,6 +38,31 @@ def tabla():
 @app.route("/menu")
 def menu():
     return render_template('menu.html')
+
+@app.route("/get_product", methods=['GET', 'POST'])
+def get_product():
+    select = request.form.get('producto')
+
+    conexion = mysql.connector.connect(
+    user="root",
+    password="",  # Asegúrate de que aquí va tu contraseña real, si la hay
+    host="localhost",
+    database="bases_Sunshine",
+    port="3306"
+)   
+    df = pd.read_sql('SELECT * FROM flor_en_transito', con=conexion)
+    Productos = " <option value="  + df["Producto"].unique() + ">" + df["Producto"].unique() + "</option>"
+    df = df[df["Producto"] == select]     
+    df = df[df["Flor Por Ingresar"] != 0]
+
+    df = df.groupby(['Producto', 'Color', 'Grado'])['Flor Por Ingresar'].sum()
+    print(df)
+    df = pd.DataFrame(df)
+    df_pivoteado = df.pivot_table(index=['Producto', 'Color'], columns='Grado',values='Flor Por Ingresar', aggfunc='sum', fill_value=0)                
+    tabla_html = df_pivoteado.to_html()
+    conexion.close()
+    return render_template('tabla.html', df = tabla_html, prod = Productos, select = select )
+
 
 
 if __name__ == '__main__':
